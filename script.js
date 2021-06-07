d9e2bafacc591f95b415a73e9dba90866a639f2e
MaldonadoAbril <abrilmld@gmail.com>
2021-06-07 14:27:22 -0300

    CLASE 3

---
+++ script.js
@@ -0,0 +1,148 @@
+/*
+////EJERCICIO 0////
+
+let nombre = prompt ("Ingrese su nombre");
+console.log (nombre);
+alert ("Hola" + " " + nombre);
+let apellido = prompt("Ingrese su apellido");
+console.log ("Hola" + " " + nombre + " " + apellido); 
+
+////EJERCICIO 1////
+
+let num1 = prompt ("Ingrese un número");
+let num2 = prompt ("Ingrese otro número");
+console.log (parseInt(num1)+parseInt(num2)); 
+
+
+////EJERCICIO 2////
+
+let edad = prompt ("Ingrese su edad");
+const año = 2021;
+let resultado = año - edad;
+console.log ("Usted nació en " + resultado); 
+
+
+////EJERCICIO 3////
+
+
+alert (2000);
+let retiro = prompt ("¿Cuanto dinero desea retirar?");
+alert (2000 - retiro); 
+
+
+////EJERCICIO 4////
+
+
+let nota1 = parseInt(prompt ("Ingrese la primer nota"));
+let nota2 = parseInt(prompt ("Ingrese la segunda nota"));
+let nota3 = parseInt(prompt ("Ingrese la tercera nota"));
+let promedio = (nota1 + nota2 + nota3) / 3;
+console.log (promedio);  
+
+
+////EJERCICIO 5////
+
+
+let metros = parseInt(prompt ("Ingrese una medida en metros"));
+let pies = (metros * 3,28084);
+let centímetros = (metros * 100);
+let pulgadas = (metros * 39,3701);
+console.log ("La medida en pies es " + pies);
+console.log ("La medida en centímetros es " + centímetros);
+console.log ("La medida en pulgadas es " + pulgadas); 
+
+////EJERCICIO 6////
+
+let num1 = parseInt (prompt ("Ingrese un número"));
+let num2 = parseInt (prompt ("Ingrese otro número"));
+alert (num1 + num2);
+let resultado = num1 + num2;
+let num3 = parseInt (prompt ("Ingrese un tercer número"));
+alert (resultado * num3);  
+
+
+////EJERCICIO 7////
+
+let minuto = parseInt (prompt ("Ingrese un número en minutos"));
+let segundos = (minuto * 60);
+alert (segundos);  
+
+
+////EJERCICIO 8////
+
+let base = parseInt(prompt("Ingrese la medida de la base de un rectángulo"));
+let altura = parseInt(prompt("Ingrese la medida de la altura de un rectángulo"));
+alert (base*altura);  
+
+
+////EJERCICIO 9////
+
+let base = parseInt(prompt("Ingrese la medida de la base de un triángulo equilatero"));
+let altura = parseInt(prompt("Ingrese la medida de la altura de un triángulo equilatero"));
+alert (base*altura*2);  
+
+
+////EJERCICIO 10////
+
+let monto = parseInt(prompt("Ingrese un monto"));
+let descuento = parseInt(prompt("Ingrese un número de descuento"));
+let descuento1 = parseInt(monto * descuento / 100);
+alert (monto - descuento1);  
+
+
+////EJERCICIO 11////
+
+let nombre = prompt("Ingrese su nombre");
+let edad = parseInt(prompt("Ingrese la edad que cumple/cumplió en 2021"));
+let año = parseInt(2021 - edad);
+alert ("Hola " + nombre + " usted nació en " + año);  
+
+////EJERCICIO 12////
+
+let nombre = prompt("Ingrese su nombre");
+let temperatura = parseInt(prompt("Ingrese una tempreratura en Celsius"));
+let conversor = parseInt((temperatura * 9/5) + 32);
+console.log ("Hola " + nombre + ", la temperatura que ingresaste en Celsius es igual a " + conversor + " Fahrenheit"); 
+
+
+////EJERCICIO 13////
+
+let nombre = prompt("Ingrese su nombre");
+let temperatura = parseInt(prompt("Ingrese una temperatura en Fahrenheit"));
+let conversor = parseInt((temperatura - 32) * 5/9);
+console.log ("Hola " + nombre + ", la temperatra que ingresaste en Fahrenheit es igual a " + conversor + " Celsius"); */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
